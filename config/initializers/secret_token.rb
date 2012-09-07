c6367671b3dacbbe929bf9483c478b3d930afb1d
marko.siftar@gmail.com <sifi@ubuntu.(none)>
2012-09-07 17:15:23 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NineGag::Application.config.secret_token = 'd76533d6a415d5f2a06cb4e05181d1eae8d3b4feddebb446387003b0135d5ea7d38b0d4872cbd51ba8d0c29db49e58a86baad974fa169c60dd72f235b4e21012'
